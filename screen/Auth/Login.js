e596a89ec1b305803a9ba515755eeac07a1c1b62
hardNormalEasyMode <petequdwns12@naver.com>
2023-10-08 01:41:38 +0900

    Create LoginScreen's form

--- screen/Auth/Login.js
+++ screen/Auth/Login.js
@@ -1,21 +1,50 @@
-import * as React from "react";
+import React, { useState, useEffect, createRef } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput } from 'react-native';
 
-export default function App() {
+// 스타일 import
+import LoginStyle from "../../styles/Auth/LoginStyle";
+
+export default function LoginScreen() {
+    const idInputRef = createRef();
+    const pwInputRef = createRef();
+
     return (
-      <View style={styles.container}>
-        <Text>테스트 중인 명집사</Text>
-        <StatusBar style="auto" />
+      <View style={LoginStyle.container}>
+        <View style={LoginStyle.titleContainer}>
+            <Text style={LoginStyle.title}>명집사</Text>
+        </View>
+        <View style={LoginStyle.imageContainer}>
+
+        </View>
+        <View style={LoginStyle.inputContainer}>
+            <TextInput 
+                style={LoginStyle.input}
+                ref={idInputRef}
+                placeholder="Username"
+                autoCapitalize="none"
+                blurOnSubmit={false}
+                returnKeyType="next"
+            />
+            <TextInput 
+                style={LoginStyle.input}
+                ref={pwInputRef}
+                placeholder="password"
+                autoCapitalize="none"
+                secureTextEntry={true}
+            />
+            <TouchableOpacity style={LoginStyle.loginButton}>
+                <Text style={LoginStyle.loginText}>로그인</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={LoginStyle.subContainer}>
+            <TouchableOpacity style={LoginStyle.registerButton}>
+                <Text style={LoginStyle.registerText}>회원가입</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={LoginStyle.findContainer}>
+                <Text style={LoginStyle.registerText}>아이디/비밀번호 찾기</Text>
+            </TouchableOpacity>
+        </View>
       </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#FFF',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
---
+++ styles/Auth/LoginStyle.js
@@ -0,0 +1,90 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+
+    titleContainer: {
+        width:"100%",
+        height: "25%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 60,
+        fontWeight: 'bold',
+        color: "skyblue"
+    },
+
+    imageContainer: {
+        width:"100%",
+        height: "35%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    inputContainer: {
+        width:"100%",
+        height: "25%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    input: {
+        height: 40,
+        width: 280,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: "gray",
+        margin: 5
+    },
+    loginButton: {
+        height: 40,
+        width: 200,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: " #dcdcdc",
+        marginTop: 20,
+        borderRadius: 10,
+    },
+    loginText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
+    subContainer: {
+        width:"100%",
+        height: "15%",
+        justifyContent: "center",
+        flexDirection: "row"
+    },
+    registerButton: {
+        height: 40,
+        width: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: "gray",
+        marginHorizontal: 10
+    },
+    registerText: {
+        fontSize: 15,
+        color: "gray"
+    },
+    findContainer: {
+        height: 40,
+        width: 170,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: "gray",
+        marginHorizontal: 10
+    },
+  });
+
+export default styles;
