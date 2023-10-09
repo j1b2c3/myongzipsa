0070867e3dda66a6d964b603800c7f6b7d2db04e
hardNormalEasyMode <petequdwns12@naver.com>
2023-10-09 15:54:49 +0900

    Home MemberShip page frame

--- screen/InApp/Home.js
+++ screen/InApp/Home.js
@@ -7,9 +7,27 @@ import { Text, View, TouchableOpacity, TextInput } from 'react-native';
 export default function HomeScreen() {
     return (
       <View>
+        {/* 명집사의 특색 */}
         <View>
-            <Text>홈화면</Text>
+          <Text>안녕하세요 명집사입니다. </Text>
+          <Text>OOO님 오늘도 좋은 하루 보내시길 바랍니다.</Text>
         </View>
+
+        {/* 광고 배너 부분 */}
+        <View>
+        
+        </View>
+
+        {/* 현재 이용 상황 */}
+        <View>
+
+        </View>
+
+        {/* 세탁기 건조기 부분(중간고사) 헬스장(기말), 주차장(미정) */}
+        <View>
+
+        </View>
+
       </View>
     );
 }
