29a19a8ce5820ce9f321d1a1ba586213e0c7f79b
Jungbin Choi <j1b2c3@naver.com>
2023-10-10 18:54:21 +0900

    back

---
+++ javascripts/firebaseConfig.js
@@ -0,0 +1,33 @@
+import firebase from "firebase/compat/app"
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "./ApiKeys";
+import "firebase/compat/auth"
+import "firebase/compat/database"
+import "firebase/compat/firestore"
+import "firebase/compat/functions"
+import  "firebase/compat/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
+
+ 
+ // export const auth = getAuth(app); // 코드 추가
+
+  export const RealTimeDatabase=firebase.database();
+  export const firestoreDatabase=firebase.firestore();
+  export const authService=firebase.auth();
+  export const storageService=firebase.storage();
+ // export const app=firebase.app();
+// if(!firebase.app.length){
+//    firebase.app.initializeApp(firebaseConfig);
