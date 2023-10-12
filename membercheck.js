9879d63e13c25b6f4ea896b7f7b653b67f973260
Jungbin Choi <j1b2c3@naver.com>
2023-10-12 12:37:14 +0900

    error clear

--- membercheck.js
+++ membercheck.js
@@ -1,17 +1,12 @@
-import firebase from './javascripts/firebaseConfig';
+import firebase from '/javascripts/firebaseConfig';
 
-// ...
 
 const checkUserInfo = () => {
     const user = firebase.auth().currentUser;
 
     if (user) {
         console.log('User is signed in:', user);
-        console.log('User UID:', user.uid);
-        console.log('User email:', user.email);
-        console.log('User display name:', user.displayName);
-        console.log('User photo URL:', user.photoURL);
-        // 기타 사용자 정보도 이곳에서 확인할 수 있습니다.
+        console.log('User name:', user.name);
     } else {
         console.log('No user is signed in.');
     }
