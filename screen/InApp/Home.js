e9c2f17189c9fa42add2f23b3fdd6a8701a84d47
Jungbin Choi <j1b2c3@naver.com>
2023-10-17 18:22:27 +0900

    123

--- screen/InApp/Home.js
+++ screen/InApp/Home.js
@@ -1,54 +1,11 @@
-<<<<<<< HEAD
-import React, { useState, useEffect, createRef } from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { Text, View, TouchableOpacity, TextInput } from 'react-native'
-import 'firebase/database'
-import { database } from '../../javascripts/FirebaseConfigFile';
-=======
 import React, { useState, useEffect, createRef } from "react";
 // import { StatusBar } from 'expo-status-bar';
 import 'firebase/database';
 import { Text, View, TouchableOpacity, TextInput, Image, StatusBar } from 'react-native';
 
->>>>>>> origin/main
 // 스타일 import
-import HomeStyle from '../../styles/Auth/HomeStyle'
+import HomeStyle from "../../styles/Auth/HomeStyle";
 
-<<<<<<< HEAD
-export default function HomeScreen ({ navigation }) {
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerStyle: HomeStyle.head_navigation,
-      headerTitleStyle: HomeStyle.title,
-      headerLeft: () => (
-        // 뒤로가기 버튼 아이콘 설정
-        <Text style={HomeStyle.backButton} onPress={() => navigation.goBack()}>
-          {'〈'} {/* 뒤로가기 아이콘 */}
-        </Text>
-      ),
-      headerTitleAlign: 'center'
-    })
-  }, [navigation])
-
-  const fetchDataFromFirebase = async () => {
-    try {
-      const snapshot = await database.ref('users/your_user_id/name').once('value');
-      const fetchedName = snapshot.val();
-      setName(fetchedName);
-    } catch (error) {
-      console.error('Error fetching data from Firebase:', error);
-    }
-  };
-
-  fetchDataFromFirebase()
-
-  return (
-    <View>
-      {/* 명집사의 특색 */}
-      <View>
-        <Text>안녕하세요 명집사입니다. </Text>
-        <Text>{`오늘도 좋은 하루 보내시길 바랍니다.`}</Text>
-=======
 export default function HomeScreen({navigation}) {
     // React.useLayoutEffect(() => {
     //   navigation.setOptions({
@@ -165,17 +122,6 @@ export default function HomeScreen({navigation}) {
           </TouchableOpacity>
         </View>
 
->>>>>>> origin/main
       </View>
-
-      {/* 광고 배너 부분 */}
-      <View></View>
-
-      {/* 현재 이용 상황 */}
-      <View></View>
-
-      {/* 세탁기 건조기 부분(중간고사) 헬스장(기말), 주차장(미정) */}
-      <View></View>
-    </View>
-  )
+    );
 }
