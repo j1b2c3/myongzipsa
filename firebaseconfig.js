62cb81fcfba8bdf523c0e8ef3481b08b44d4bd1d
Jungbin Choi <j1b2c3@naver.com>
2023-10-17 16:54:33 +0900

    test1

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -7,16 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyCz4vQbdJ2yYmc1Mb0x6gC_FAAKkPIBaUw",
+  authDomain: "myongjipsa.firebaseapp.com",
+  databaseURL: "https://myongjipsa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "myongjipsa",
+  storageBucket: "myongjipsa.appspot.com",
+  messagingSenderId: "196372437968",
+  appId: "1:196372437968:web:fbb75ff24583af24af77d5",
+  measurementId: "G-7Q77CBC791"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
