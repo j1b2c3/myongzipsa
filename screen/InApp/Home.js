87f3e50292bc83ecdfdfdf6c1954ed6685ab4417
Jungbin Choi <j1b2c3@naver.com>
2023-10-24 19:18:23 +0900

    washingMchart 수정중

--- screen/InApp/Home.js
+++ screen/InApp/Home.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect, createRef } from "react";
 import 'firebase/database';
 import { Text, View, TouchableOpacity, TextInput, Image, StatusBar } from 'react-native';
 import { auth, database } from '../../javascripts/FirebaseConfigFile'
-// 스타일 import
 import HomeStyle from "../../styles/Auth/HomeStyle";
+import ChartScreen from "../InApp/Chart";
 
 export default function HomeScreen({ navigation }) {
   const [name, setName] = useState('')
@@ -20,116 +20,114 @@ export default function HomeScreen({ navigation }) {
       })
     }
   }, [])
-  import ChartScreen from "../InApp/Chart";
 
-  export default function HomeScreen({ navigation }) {
-    return (
-      <View style={HomeStyle.container}>
-        <StatusBar />
-        {/*명집사 */}
-        <View style={HomeStyle.titleContainer}>
-          {/* <Image 
+  return (
+    <View style={HomeStyle.container}>
+      <StatusBar />
+      {/*명집사 */}
+      <View style={HomeStyle.titleContainer}>
+        {/* <Image 
             style={HomeStyle.title}
             source = {require("../../img/icon.png")}
           /> */}
-          <Text style={HomeStyle.titleText}>명 집 사</Text>
-        </View>
+        <Text style={HomeStyle.titleText}>명 집 사</Text>
+      </View>
 
-        {/* 명집사의 특색 */}
-        <View style={HomeStyle.zipsaContainer}>
-          <Text style={HomeStyle.zipsa}>안녕하세요 명집사입니다. </Text>
-          <Text style={HomeStyle.zipsa}>{name}님 오늘도 좋은 아침입니다.</Text>
-          <Text style={HomeStyle.zipsa}>무엇을 도와드릴까요?</Text>
-        </View>
+      {/* 명집사의 특색 */}
+      <View style={HomeStyle.zipsaContainer}>
+        <Text style={HomeStyle.zipsa}>안녕하세요 명집사입니다. </Text>
+        <Text style={HomeStyle.zipsa}>{name}님 오늘도 좋은 아침입니다.</Text>
+        <Text style={HomeStyle.zipsa}>무엇을 도와드릴까요?</Text>
+      </View>
 
-        {/* 현재 이용 상황 */}
-        <View style={HomeStyle.usingContainer}>
-          <TouchableOpacity style={HomeStyle.firstButton}>
-            <Text style={HomeStyle.useBtext}>세탁기</Text>
-          </TouchableOpacity>
+      {/* 현재 이용 상황 */}
+      <View style={HomeStyle.usingContainer}>
+        <TouchableOpacity style={HomeStyle.firstButton}>
+          <Text style={HomeStyle.useBtext}>세탁기</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={HomeStyle.secondButton}>
-            <Text style={HomeStyle.useBtext}>건조기</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={HomeStyle.secondButton}>
+          <Text style={HomeStyle.useBtext}>건조기</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={HomeStyle.thirdButton}>
-            <Text style={HomeStyle.useBtext}>헬스장</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={HomeStyle.thirdButton}>
+          <Text style={HomeStyle.useBtext}>헬스장</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={HomeStyle.finalButton}>
-            <Text style={HomeStyle.useBtext}>주차장</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity style={HomeStyle.finalButton}>
+          <Text style={HomeStyle.useBtext}>주차장</Text>
+        </TouchableOpacity>
+      </View>
 
-        <View style={HomeStyle.usingContainer2}>
-          <TouchableOpacity style={HomeStyle.useButton}
-            onPress={() => navigation.navigate("사용현황")}
-          >
-            <Text style={HomeStyle.useBtext1}>사용중</Text>
-          </TouchableOpacity>
+      <View style={HomeStyle.usingContainer2}>
+        <TouchableOpacity style={HomeStyle.useButton}
+          onPress={() => navigation.navigate("사용현황")}
+        >
+          <Text style={HomeStyle.useBtext1}>사용중</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={HomeStyle.reserveButton}
-            onPress={() => navigation.navigate("예약현황")}
-          >
-            <Text style={HomeStyle.useBtext1}>예약중</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={HomeStyle.recommendButton}>
-            <Text style={HomeStyle.useBtext1}>추천</Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* 세탁기 건조기 부분(중간고사) 헬스장(기말), 주차장(미정) */}
-        <View style={HomeStyle.iconContainer}>
-          {/*세탁기*/}
-          <TouchableOpacity style={HomeStyle.leftButton}
-            onPress={() => navigation.navigate("배치도")}>
-            <Image style={HomeStyle.icon}
-              source={require("../../img/icon_washing_machine.jpg")} />
-            <Text>세탁기</Text>
-            {/* onPress = */}
-          </TouchableOpacity>
-
-          {/*건조기*/}
-          <TouchableOpacity style={HomeStyle.rightButton}
-            onPress={() => navigation.navigate("배치도")}>
-            <Image style={HomeStyle.icon}
-              source={require("../../img/icon_drying_machine.jpg")} />
-            <Text>건조기</Text>
-            {/* onPress = */}
-          </TouchableOpacity>
-        </View>
-
-        <View style={HomeStyle.iconContainer2}>
-          {/*헬스장*/}
-          <TouchableOpacity style={HomeStyle.leftButton}>
-            <Image style={HomeStyle.icon2}
-              source={require("../../img/icon_gym.jpg")} />
-            <Text>헬스장</Text>
-            {/* onPress = */}
-          </TouchableOpacity>
-
-          {/*주차장*/}
-          <TouchableOpacity style={HomeStyle.rightButton}>
-            <Image style={HomeStyle.icon2}
-              source={require("../../img/icon_parking.jpg")} />
-            <Text>주차장</Text>
-            {/* onPress = */}
-          </TouchableOpacity>
-        </View>
-
-        {/* 광고 배너 부분 */}
-        <View style={HomeStyle.advContainer}>
-          <TouchableOpacity >
-            <Image style={HomeStyle.advert}
-              source={require("../../img/blank.png")} />
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity style={HomeStyle.reserveButton}
+          onPress={() => navigation.navigate("예약현황")}
+        >
+          <Text style={HomeStyle.useBtext1}>예약중</Text>
+        </TouchableOpacity>
 
         <TouchableOpacity style={HomeStyle.recommendButton}>
           <Text style={HomeStyle.useBtext1}>추천</Text>
-          {/* onPress = 세탁기 빠른 예약 함수 */}
         </TouchableOpacity>
       </View>
-    )
-  }
+
+      {/* 세탁기 건조기 부분(중간고사) 헬스장(기말), 주차장(미정) */}
+      <View style={HomeStyle.iconContainer}>
+        {/*세탁기*/}
+        <TouchableOpacity style={HomeStyle.leftButton}
+          onPress={() => navigation.navigate("배치도")}>
+          <Image style={HomeStyle.icon}
+            source={require("../../img/icon_washing_machine.jpg")} />
+          <Text>세탁기</Text>
+          {/* onPress = */}
+        </TouchableOpacity>
+
+        {/*건조기*/}
+        <TouchableOpacity style={HomeStyle.rightButton}
+          onPress={() => navigation.navigate("배치도")}>
+          <Image style={HomeStyle.icon}
+            source={require("../../img/icon_drying_machine.jpg")} />
+          <Text>건조기</Text>
+          {/* onPress = */}
+        </TouchableOpacity>
+      </View>
+
+      <View style={HomeStyle.iconContainer2}>
+        {/*헬스장*/}
+        <TouchableOpacity style={HomeStyle.leftButton}>
+          <Image style={HomeStyle.icon2}
+            source={require("../../img/icon_gym.jpg")} />
+          <Text>헬스장</Text>
+          {/* onPress = */}
+        </TouchableOpacity>
+
+        {/*주차장*/}
+        <TouchableOpacity style={HomeStyle.rightButton}>
+          <Image style={HomeStyle.icon2}
+            source={require("../../img/icon_parking.jpg")} />
+          <Text>주차장</Text>
+          {/* onPress = */}
+        </TouchableOpacity>
+      </View>
+
+      {/* 광고 배너 부분 */}
+      <View style={HomeStyle.advContainer}>
+        <TouchableOpacity >
+          <Image style={HomeStyle.advert}
+            source={require("../../img/blank.png")} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={HomeStyle.recommendButton}>
+        <Text style={HomeStyle.useBtext1}>추천</Text>
+        {/* onPress = 세탁기 빠른 예약 함수 */}
+      </TouchableOpacity>
+    </View>
+  )
+}
--- screen/InApp/WashingMchart.js
+++ screen/InApp/WashingMchart.js
@@ -1,112 +1,199 @@
-import React, { useState, useEffect, createRef } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, TouchableOpacity, TextInput, Button, Alert, StyleSheet } from 'react-native';
-
-//스타일 import
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity, Text, Alert, TextInput } from 'react-native';
+import firebase from 'firebase';
 import WashingMchartStyle from "../../styles/Auth/WashingMchartStyle";
 
-export default function WashingMchartScreen({navigation}) {
+const WashingMchartScreen = () => {
+    const [washingMachines, setWashingMachines] = useState({});
+    const [machineNumber, setMachineNumber] = useState(null);
+    const [remainingTimeInput, setRemainingTimeInput] = useState(null);
+
+    // Firebase Realtime Database 연결 설정
+    useEffect(() => {
+        const databaseRef = firebase.database().ref('washingMachines');
+
+        // 데이터베이스에서 초기 데이터를 가져옵니다.
+        databaseRef.on('value', (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setWashingMachines(data);
+            }
+        });
+
+        // Firebase Realtime Database와의 연결 해제
+        return () => {
+            databaseRef.off();
+        };
+    }, []);
+
+    const handleMachineClick = (machineNumber) => {
+        if (!washingMachines[machineNumber]) {
+            // 세탁기가 없는 경우
+            Alert.alert(`세탁기 ${machineNumber}는 존재하지 않습니다.`);
+        } else if (washingMachines[machineNumber].available) {
+            // 세탁기가 사용 가능한 경우 남은 시간 입력 폼을 표시
+            setMachineNumber(machineNumber);
+        } else {
+            // 세탁기가 사용 중인 경우 예약 여부를 묻는 알림 표시
+            Alert.alert(
+                `세탁기 ${machineNumber}`,
+                `남은 시간: ${washingMachines[machineNumber].remainingTime}분\n예약하시겠습니까?`,
+                [
+                    {
+                        text: "예약",
+                        onPress: () => reserveMachine(machineNumber, 'user1'),
+                    },
+                    {
+                        text: "취소",
+                        onPress: () => console.log("취소"),
+                        style: "cancel",
+                    },
+                ]
+            );
+        }
+    };
+
+    const reserveMachine = (machineNumber, userId) => {
+        const initialRemainingTime = parseInt(remainingTimeInput, 10) || 30; // 입력된 남은 시간 또는 기본 값 (예: 30분)
+
+        firebase.database().ref(`washingMachines/${machineNumber}`).update({
+            available: false,
+            reservedBy: userId,
+            remainingTime: initialRemainingTime
+        });
+
+        if (initialRemainingTime <= 5) {
+            Alert.alert(`세탁기 ${machineNumber} 예약이 완료되었습니다. 남은 시간: ${initialRemainingTime}분. (5분 이하)`);
+        } else {
+            Alert.alert(`세탁기 ${machineNumber} 예약이 완료되었습니다. 남은 시간: ${initialRemainingTime}분.`);
+        }
+
+        // 남은 시간을 감소시키는 타이머 시작
+        const timer = setInterval(() => {
+            const updatedRemainingTime = washingMachines[machineNumber].remainingTime - 1;
+            firebase.database().ref(`washingMachines/${machineNumber}`).update({
+                remainingTime: updatedRemainingTime
+            });
+
+            if (updatedRemainingTime <= 5) {
+                Alert.alert(`세탁기 ${machineNumber} 남은 시간: ${updatedRemainingTime}분. (5분 이하)`);
+            }
+
+            if (updatedRemainingTime <= 0) {
+                clearInterval(timer);
+            }
+        }, 60000); // 1분마다 감소 (60,000 밀리초)
+
+        // 남은 시간 입력 폼 초기화
+        setMachineNumber(null);
+        setRemainingTimeInput(null);
+    };
+
+    const autoReserveMachine = (userId) => {
+        let availableMachines = Object.entries(washingMachines)
+            .filter(([_, machine]) => machine.available && machine.remainingTime !== null)
+            .sort(([, a], [, b]) => a.remainingTime - b.remainingTime);
+
+        if (availableMachines.length > 0) {
+            // 사용 가능한 세탁기가 있을 때
+            const [machineNumber] = availableMachines[0];
+            Alert.alert(`사용 가능한 세탁기: ${machineNumber}`);
+        } else {
+            // 사용 가능한 세탁기가 없을 때
+            const shortestTimeMachine = Object.entries(washingMachines)
+                .filter(([_, machine]) => machine.remainingTime !== null)
+                .sort(([, a], [, b]) => a.remainingTime - b.remainingTime);
+
+            if (shortestTimeMachine.length > 0) {
+                const [machineNumber] = shortestTimeMachine[0];
+                reserveMachine(machineNumber, userId);
+            } else {
+                Alert.alert('예약 가능한 세탁기가 없습니다.');
+            }
+        }
+    };
+
     return (
         <View>
-            <View style={WashingMchartStyle.iconContainer}>
-                {/*세탁기1*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
-                    <Text>1번 세탁기</Text>
-                </TouchableOpacity>
+            {machineNumber !== null ? (
+                // 남은 시간 입력 폼 표시
+                <View style={WashingMchartStyle.iconContainer}>
+                    <TextInput
+                        placeholder="남은 시간 (분)"
+                        onChangeText={(text) => setRemainingTimeInput(text)}
+                        keyboardType="numeric"
+                    />
+                    <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => reserveMachine(machineNumber, 'user1')}>
+                        <Text>예약</Text>
+                    </TouchableOpacity>
+                </View>
+            ) : (
+                // 세탁기 상태 표시
+                <View style={WashingMchartStyle.iconContainer}>
+                    <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('1')}>
+                        <Text>1번 세탁기</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('4')}>
+                        <Text>4번 세탁기</Text>
+                    </TouchableOpacity>
+                </View>
 
-                {/*세탁기4*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
-                    <Text>4번 세탁기</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style={WashingMchartStyle.iconContainer}>
-                {/*세탁기2*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                                <View style={WashingMchartStyle.iconContainer}>
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('2', 'user1')}>
                     <Text>2번 세탁기</Text>
                 </TouchableOpacity>
-
-                {/*세탁기5*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('5', 'user1')}>
                     <Text>5번 세탁기</Text>
                 </TouchableOpacity>
             </View>
 
             <View style={WashingMchartStyle.iconContainer}>
-                {/*세탁기3*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('3', 'user1')}>
                     <Text>3번 세탁기</Text>
                 </TouchableOpacity>
-
-                {/*세탁기6*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('6', 'user1')}>
                     <Text>6번 세탁기</Text>
                 </TouchableOpacity>
             </View>
 
             <View style={WashingMchartStyle.iconContainer}>
-                {/*건조기1*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
-                    <Text>1번 건조기</Text>
-                </TouchableOpacity>
-
-                {/*세탁기7*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('7', 'user1')}>
                     <Text>7번 세탁기</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={WashingMchartStyle.rightButton}>
+                    <Text>1번 건조기</Text>
+                </TouchableOpacity>
             </View>
 
             <View style={WashingMchartStyle.iconContainer}>
-                {/*건조기2*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
-                    <Text>2번 건조기</Text>
-                </TouchableOpacity>
-
-                {/*세탁기8*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => handleMachineClick('8', 'user1')}>
                     <Text>8번 세탁기</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={WashingMchartStyle.rightButton}>
+                    <Text>2번 건조기</Text>
+                </TouchableOpacity>
             </View>
 
             <View style={WashingMchartStyle.iconContainer}>
-                {/*입구*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => autoReserveMachine('user1')}>
                     <Text>자동 추천</Text>
                 </TouchableOpacity>
-
-                {/*건조기3*/}
                 <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
                     <Text>3번 건조기</Text>
                 </TouchableOpacity>
             </View>
 
             <View style={WashingMchartStyle.iconContainer}>
-                {/*입구*/}
                 <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
-                    <Text>입구</Text>
-                </TouchableOpacity>
-
-                {/*건조기4*/}
-                <TouchableOpacity style={WashingMchartStyle.rightButton}>
-                {/* onPress={() => navigation.navigate("배치도")}> */}
                     <Text>4번 건조기</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={WashingMchartStyle.rightButton} onPress={() => Alert.alert('입구입니다.')}>
+                    <Text>입구</Text>
                 </TouchableOpacity>
             </View>
         </View>
-
     );
-}
+};)
 
+
+export default WashingMchartScreen;
