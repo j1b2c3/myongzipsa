ce30efdc274a4271c92d065fac63686c93b7346e
daeryuk <eofbrdlehdtjf@naver.com>
2023-10-24 17:43:03 +0900

    비밀번호 찾기 UI 연동 코드만 추가했음,

--- screen/InApp/FindPwd.js
+++ screen/InApp/FindPwd.js
@@ -1,34 +1,41 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
-import { auth } from '../../javascripts/FirebaseConfigFile';
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native'
+import { auth } from '../../javascripts/FirebaseConfigFile'
 
+import FindPwdStyle from '../../styles/Auth/FindPwdStyle'
 const FindPwdScreen = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('')
 
   const handlePasswordRecovery = () => {
-    auth.sendPasswordResetEmail(email)
+    auth
+      .sendPasswordResetEmail(email)
       .then(() => {
-        Alert.alert('비밀번호 재설정 이메일이 전송되었습니다.');
+        Alert.alert('비밀번호 재설정 이메일이 전송되었습니다.')
       })
-      .catch((error) => {
-        Alert.alert('비밀번호 재설정 오류');
-        console.error('Error finding ID:', error);
-
-      });
-  };
+      .catch(error => {
+        Alert.alert('비밀번호 재설정 오류')
+        console.error('Error finding ID:', error)
+      })
+  }
 
   return (
-    <View>
-      <TextInput
-        placeholder="이메일을 입력하세요"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TouchableOpacity onPress={handlePasswordRecovery}>
-        <Text>비밀번호 찾기</Text>
-      </TouchableOpacity>
+    <View style={FindPwdStyle.container}>
+      <View style={FindPwdStyle.inputContainer}>
+        <TextInput
+          style={FindPwdStyle.input}
+          placeholder='이메일을 입력하세요'
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={FindPwdStyle.buttonContainer}>
+        <TouchableOpacity onPress={handlePasswordRecovery}>
+          <Text>비밀번호 찾기</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  );
-};
+  )
+}
 
-export default FindPwdScreen;
+export default FindPwdScreen
+
