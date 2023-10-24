ad1e5f930448e64f37cf126c3a7535a201948fa1
Jungbin Choi <j1b2c3@naver.com>
2023-10-24 17:55:27 +0900

    미완성

--- screen/InApp/FindID.js
+++ screen/InApp/FindID.js
@@ -1,7 +1,7 @@
-
 import React, { useState } from "react";
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import firebase from "firebase/app";
+import 'firebase/auth';
 import 'firebase/database';
 import { auth, database } from '../../javascripts/FirebaseConfigFile';
 
@@ -13,25 +13,35 @@ const FindIDScreen = () => {
     const [name, setName] = useState('');
 
     const FindID = () => {
+        // // Firebase 앱 초기화
+        // if (!firebase.apps.length) {
+        //     firebase.initializeApp(FirebaseConfigFile);
+        // }
+
         let userRef = database.ref('users');
         userRef
             .orderByChild('phoneNumber')
             .equalTo(phoneNumber)
             .once('value')
             .then((snapshot) => {
+                let found = false;
                 snapshot.forEach((childSnapshot) => {
                     let user = childSnapshot.val();
                     if (user.name === name) {
+                        found = true;
                         let email = user.email;
                         Alert.alert(`아이디는 ${email}입니다.`);
                     }
                 });
+                if (!found) {
+                    Alert.alert(`일치하는 정보가 없습니다.`);
+                }
             })
             .catch((error) => {
                 Alert.alert(`아이디 찾기 오류`);
                 console.error('Error finding ID:', error);
             });
-    }
+    };
 
     return (
         <View style={FindIDStyle.container}>
@@ -58,6 +68,6 @@ const FindIDScreen = () => {
             </View>
         </View>
     );
-}
+};
 
 export default FindIDScreen;
