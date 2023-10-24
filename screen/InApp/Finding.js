e614c8b050ba8ab9029b69c3a653ecdf222e05d2
Jungbin Choi <j1b2c3@naver.com>
2023-10-24 18:16:40 +0900

    test

--- screen/InApp/Finding.js
+++ screen/InApp/Finding.js
@@ -6,13 +6,13 @@ import FindIDScreen from './FindID';
 import FindPwdScreen from './FindPwd';
 
 const Tab = createMaterialTopTabNavigator();
- 
+
 //FindingScreen 상단탭바에 FindingIDScreen,FindingPwdScreen 추가
 export default function FindingScreen() {
-    return (
-      <Tab.Navigator initialRouteName="아이디찾기">
-        <Tab.Screen name="아이디찾기" component={FindIDScreen} options={{ title: '아이디 찾기' }} />
-        <Tab.Screen name="비밀번호찾기" component={FindPwdScreen} options={{ title: '비밀번호 찾기' }} />
-      </Tab.Navigator>
-    );
-  }
+  return (
+    <Tab.Navigator initialRouteName="아이디찾기">
+      <Tab.Screen name="아이디찾기" component={FindIDScreen} options={{ title: '아이디 찾기' }} />
+      <Tab.Screen name="비밀번호찾기" component={FindPwdScreen} options={{ title: '비밀번호 찾기' }} />
+    </Tab.Navigator>
+  );
+}
