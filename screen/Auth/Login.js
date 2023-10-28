c4d1aadcd1336a30e67d947e703a6cea65dc4c6b
hardNormalEasyMode <petequdwns12@naver.com>
2023-10-29 02:00:44 +0900

    Add TouchableWithoutFeedback on Login.js

--- screen/Auth/Login.js
+++ screen/Auth/Login.js
@@ -1,6 +1,6 @@
 import { auth } from '../../javascripts/FirebaseConfigFile';
 import React, { useState, useEffect, createRef } from 'react'
-import { Text, View, TouchableOpacity, TextInput, Image, Alert } from 'react-native'
+import { Text, View, TouchableOpacity, TextInput, Image, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native'
 
 // 스타일 import
 import LoginStyle from '../../styles/Auth/LoginStyle'
@@ -27,57 +27,60 @@ export default function LoginScreen({ navigation }) {
   }
 
   return (
-    <View style={LoginStyle.container}>
-      <View style={LoginStyle.titleContainer}>
-        <Text style={LoginStyle.title}>명집사</Text>
-      </View>
-      <View style={LoginStyle.imageContainer}>
-        <Image
-          style={LoginStyle.image}
-          source={require('../../img/icon_mjuM.jpg')}
-        />
-      </View>
-      <View style={LoginStyle.inputContainer}>
-        <TextInput
-          style={LoginStyle.input}
-          ref={idInputRef}
-          placeholder='   Username'
-          autoCapitalize='none'
-          blurOnSubmit={false}
-          returnKeyType='next'
-          onChangeText={text => setEmail(text)}
-        />
-        <TextInput
-          style={LoginStyle.input}
-          ref={pwInputRef}
-          placeholder='   password'
-          autoCapitalize='none'
-          secureTextEntry={true}
-          onChangeText={text => setPassword(text)}
-        />
-        <TouchableOpacity
-          style={LoginStyle.loginButton}
-          onPress={handleLogin}
-        >
-          <Text style={LoginStyle.loginText}>로그인</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={LoginStyle.container}>
+        <View style={LoginStyle.titleContainer}>
+          <Text style={LoginStyle.title}>명집사</Text>
+        </View>
+        <View style={LoginStyle.imageContainer}>
+          <Image
+            style={LoginStyle.image}
+            source={require('../../img/icon_mjuM.jpg')}
+          />
+        </View>
+        <View style={LoginStyle.inputContainer}>
+          <TextInput
+            style={LoginStyle.input}
+            ref={idInputRef}
+            placeholder='   Username'
+            autoCapitalize='none'
+            blurOnSubmit={false}
+            returnKeyType='next'
+            onChangeText={text => setEmail(text)}
+          />
+          <TextInput
+            style={LoginStyle.input}
+            ref={pwInputRef}
+            placeholder='   password'
+            autoCapitalize='none'
+            secureTextEntry={true}
+            returnKeyType='done'
+            onChangeText={text => setPassword(text)}
+          />
+          <TouchableOpacity
+            style={LoginStyle.loginButton}
+            onPress={handleLogin}
+          >
+            <Text style={LoginStyle.loginText}>로그인</Text>
 
-        </TouchableOpacity>
-      </View>
-      <View style={LoginStyle.subContainer}>
-        <TouchableOpacity
-          style={LoginStyle.registerButton}
-          onPress={() => navigation.navigate('회원가입')}
-        >
-          <Text style={LoginStyle.registerText}>회원가입</Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+        <View style={LoginStyle.subContainer}>
+          <TouchableOpacity
+            style={LoginStyle.registerButton}
+            onPress={() => navigation.navigate('회원가입')}
+          >
+            <Text style={LoginStyle.registerText}>회원가입</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={LoginStyle.findContainer}
-          onPress={() => navigation.navigate('아이디/비밀번호 찾기')}
-        >
-          <Text style={LoginStyle.registerText}>아이디/비밀번호 찾기</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={LoginStyle.findContainer}
+            onPress={() => navigation.navigate('아이디/비밀번호 찾기')}
+          >
+            <Text style={LoginStyle.registerText}>아이디/비밀번호 찾기</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
     )
 }
