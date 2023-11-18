68b44f363379a7379e9f7b341a650ffcac80b797
Jungbin Choi <j1b2c3@naver.com>
2023-11-18 14:43:45 +0900

    error check

--- styles/Auth/WashingMchartStyle.js
+++ styles/Auth/WashingMchartStyle.js
@@ -1,57 +1,57 @@
 import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
-    // <<<<<<< main
-    iconContainer: {
-        width: "100%",
-        height: "13.5%",
-        justifyContent: "flex-start",
-        flexDirection: "row",
-        //backgroundColor: 'skyblue',
-    },
+    // // <<<<<<< main
+    // iconContainer: {
+    //     width: "100%",
+    //     height: "13.5%",
+    //     justifyContent: "flex-start",
+    //     flexDirection: "row",
+    //     //backgroundColor: 'skyblue',
+    // },
 
-    leftButton: {
-        top: 40,
-        left: 40,
-        height: 80,
-        width: 120,
-        justifyContent: "center",
-        alignItems: "center",
-        borderWidth: 4,
-        borderRadius: 10,
-        borderColor: "gray",
-        marginHorizontal: 10
-    },
+    // leftButton: {
+    //     top: 40,
+    //     left: 40,
+    //     height: 80,
+    //     width: 120,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     borderWidth: 4,
+    //     borderRadius: 10,
+    //     borderColor: "gray",
+    //     marginHorizontal: 10
+    // },
 
-    leftButton1: {
-        top: 90,
-        left: 40,
-        height: 80,
-        width: 120,
-        justifyContent: "center",
-        alignItems: "center",
-        borderWidth: 4,
-        borderRadius: 10,
-        borderColor: "gray",
-        marginHorizontal: 10
-    },
+    // leftButton1: {
+    //     top: 90,
+    //     left: 40,
+    //     height: 80,
+    //     width: 120,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     borderWidth: 4,
+    //     borderRadius: 10,
+    //     borderColor: "gray",
+    //     marginHorizontal: 10
+    // },
 
-    rightButton: {
-        top: 40,
-        left: 90,
-        height: 80,
-        width: 120,
-        justifyContent: "center",
-        alignItems: "center",
-        borderWidth: 4,
-        borderRadius: 10,
-        borderColor: "gray",
-        marginHorizontal: 10
-    },
+    // rightButton: {
+    //     top: 40,
+    //     left: 90,
+    //     height: 80,
+    //     width: 120,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     borderWidth: 4,
+    //     borderRadius: 10,
+    //     borderColor: "gray",
+    //     marginHorizontal: 10
+    // },
 
-    icon: {
-        width: 135,
-        height: 150,
-    },
+    // icon: {
+    //     width: 135,
+    //     height: 150,
+    // },
     // ======= front부분
     container: {
         flex: 1,
