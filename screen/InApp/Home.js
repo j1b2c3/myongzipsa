de73e78976850eab9dc1363222e618163126b180
daeryuk <eofbrdlehdtjf@naver.com>
2023-11-21 19:07:27 +0900

    QR 코드 버튼 위로 뺌

--- screen/InApp/Home.js
+++ screen/InApp/Home.js
@@ -45,7 +45,10 @@ export default function HomeScreen({ navigation }) {
           <Text style={HomeStyle.useBtext}>헬스장</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={HomeStyle.finalButton}>
+        <TouchableOpacity style={HomeStyle.finalButton}
+        onPress={() => navigation.navigate("QR코드", {
+            pageName: "QR코드"
+          })}>
           <Text style={HomeStyle.useBtext}>QR코드</Text>
         </TouchableOpacity>
       </View>
@@ -128,13 +131,10 @@ export default function HomeScreen({ navigation }) {
         </TouchableOpacity>
 
         {/*주차장*/}
-        <TouchableOpacity style={HomeStyle.rightButton}
-        onPress={() => navigation.navigate("QR코드", {
-            pageName: "QR코드"
-          })}>
+        <TouchableOpacity style={HomeStyle.rightButton}>
           <Image style={HomeStyle.icon2}
             source={require("../../img/icon_parking.jpg")} />
-          <Text>QR코드</Text>
+          <Text>주차장</Text>
           {/* onPress = */}
         </TouchableOpacity>
       </View>
