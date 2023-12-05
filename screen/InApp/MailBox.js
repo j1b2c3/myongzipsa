70c937c3a4f9f38a74be3f45050ec1d427ccea3c
daeryuk <eofbrdlehdtjf@naver.com>
2023-12-05 16:24:44 +0900

    쪽지함 기능 구현했습니다

--- screen/InApp/MailBox.js
+++ screen/InApp/MailBox.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Text, View, TouchableOpacity, Alert, Image, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, TouchableOpacity, Alert, ScrollView, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { auth, database } from '../../javascripts/FirebaseConfigFile';
 
 //스타일 import
 import MailBoxStyle from '../../styles/Auth/MailBoxStyle';
@@ -7,27 +8,64 @@ import MailBoxStyle from '../../styles/Auth/MailBoxStyle';
 const MailBoxScreen = () => {
     const [selectedButton, setSelectedButton] = useState(null);
     const [messageText, setMessageText] = useState('');
+    const [receivedMessages, setReceivedMessages] = useState([]);
+    const [sentMessages, setSentMessages] = useState([]);
 
+    useEffect(() => {
+        getReceivedMessages();
+        getSentMessages();
+    }, []);
+
+    const userKey = auth.currentUser.email.replace(/[.$#[\]/]/g, "_");
+    const adminKey = "admin@gmail_com";
+
+    // 파이어베이스 DB에 메시지 저장
     const handleMessageSend = () => {
-        //관리자에게 전달
-        console.log("보낸 쪽지 내용:", messageText);
+        const ref = database.ref(`MailBox/${adminKey}/${userKey === adminKey ? 'sentMsg' : 'receivedMsg'}`);
+        ref.once("value", function(snapshot) {
+            const messageNumber = snapshot.numChildren() + 1; // 새로운 메시지 순서 번호
+            ref.child(messageNumber).set({
+                [userKey]: messageText
+            });
+        });
+
         setMessageText(''); // 입력 상자 초기화
     };
+    // 받은 메시지 확인하기
+    const getReceivedMessages = () => {
+        const ref = database.ref(`MailBox/${adminKey}/${userKey === adminKey ? 'receivedMsg' : 'sentMsg'}`);
+        ref.once("value", snapshot => {
+            const data = snapshot.val();
+            if (data) {
+                const messages = Object.keys(data).map(key => ({
+                    id: key,
+                    content: Object.values(data[key])[0]
+                })).reverse(); //내림차순.
+                setReceivedMessages(messages);
+            } else {
+                setReceivedMessages([{ id: '1', content: '쪽지함이 비어있습니다!' }]);
+            }
+        });
+    };
+    // 보낸 메시지 확인하기
+    const getSentMessages = () => {
+        const ref = database.ref(`MailBox/${adminKey}/${userKey === adminKey ? 'sentMsg' : 'receivedMsg'}`);
+        ref.once("value", snapshot => {
+            const data = snapshot.val();
+            if (data) {
+                const messages = Object.keys(data)
+                    .filter(key => Object.keys(data[key])[0] === (userKey === adminKey ? adminKey : userKey))
+                    .map(key => ({
+                        id: key,
+                        content: Object.values(data[key])[0]
+                    })).reverse(); // 내림차순
+                setSentMessages(messages);
+            } else {
+                setSentMessages([{ id: '1', content: '쪽지함이 비어있습니다!' }]);
+            }
+        });
+    };
 
-    // 받은 쪽지 목록 예시 데이터
-    const receivedMessages = [
-        { id: 1, content: '첫 번째 받은 쪽지' },
-        { id: 2, content: '두 번째 받은 쪽지' },
-        { id: 3, content: '세 번째 받은 쪽지' },
-        { id: 4, content: '네 번째 받은 쪽지' },
-
-    ];
-
-    // 보낸 쪽지 목록 예시 데이터
-    const sentMessages = [
-        { id: 1, content: '첫 번째 보낸 쪽지' },
-        { id: 2, content: '두 번째 보낸 쪽지' },
-    ];
 
     return (
         <KeyboardAvoidingView
@@ -43,7 +81,7 @@ const MailBoxScreen = () => {
                 </TouchableOpacity>
             </View>
 
-            <View style={MailBoxStyle.ListContainer}>
+            <ScrollView style={MailBoxStyle.ListContainer}>
                 {/* 여기에 받은 쪽지와 보낸 쪽지 목록 표시 */}
                 {(selectedButton === '받은쪽지') && (
                     <View>
@@ -55,10 +93,8 @@ const MailBoxScreen = () => {
                             </View>
                         ))}
                     </View>
-
                 )}
                 {(selectedButton === '보낸쪽지') && (
-
                     <View>
                         <Text style={MailBoxStyle.ListTitleText}>보낸 쪽지 목록</Text>
                         {/* 보낸 쪽지 목록 렌더링 */}
@@ -68,9 +104,8 @@ const MailBoxScreen = () => {
                             </View>
                         ))}
                     </View>
-
                 )}
-            </View>
+            </ScrollView>
 
             <KeyboardAvoidingView
                 behavior={Platform.OS === "ios" ? "padding" : null}
